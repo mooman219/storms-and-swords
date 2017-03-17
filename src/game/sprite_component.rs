591f33960c184f17952beb09951ae63ce1006550
Main User <tucker@localhost.localdomain>
2017-03-16 22:09:36 -0400

    Added print for glsl version

---
+++ src/game/sprite_component.rs
@@ -0,0 +1,14 @@
+use graphics::sprite::Sprite;
+use graphics::renderable::Renderable;
+use glium::Frame;
+
+pub struct SpriteComponent {
+    sprite: Sprite
+}
+/*
+impl Renderable {
+    pub fn render (&self, &mut frame: Frame) {
+        
+    }
+}
+*/
